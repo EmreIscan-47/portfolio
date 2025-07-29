5bf4e25ec59a9376e018a9c173feb9bc709ef421
EmreIscan-47 <iscan.connect@outlook.de>
2025-07-29 20:37:12 +0200

    Project structure added

---
+++ src/app/shared/all-header-components/mobile-header-menu/mobile-header-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-header-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-header-menu.component.html',
+  styleUrl: './mobile-header-menu.component.scss'
+})
+export class MobileHeaderMenuComponent {
+
+}
