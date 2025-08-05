1ac2272b570b1f812ac5dbc6e727ebdcf8f4eb61
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-05 15:16:47 +0200

    About-me section design finished (without Animation  and responsive)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './main/hero/hero.component';
+import { MyProjectsComponent } from './main/my-projects/my-projects.component';
+import { AboutMeComponent } from './main/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, MyProjectsComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
