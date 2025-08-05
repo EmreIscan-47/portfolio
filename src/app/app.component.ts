566ed54544bcb767b66aad3973e0a7e6dacb1765
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-05 17:37:14 +0200

    Skills-section design finished (Without animation and responsiveness)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './main/hero/hero.component';
 import { MyProjectsComponent } from './main/my-projects/my-projects.component';
 import { AboutMeComponent } from './main/about-me/about-me.component';
+import { SkillsComponent } from './main/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, MyProjectsComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, MyProjectsComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
