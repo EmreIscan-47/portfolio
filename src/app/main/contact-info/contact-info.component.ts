1be4777ae2994ce0a527188ad2d673d321911e63
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-07 18:41:11 +0200

    Contact-Info and Footer design finished (Without animations and responsiveness)

--- src/app/main/contact-info/contact-info.component.ts
+++ src/app/main/contact-info/contact-info.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FooterComponent } from '../../shared/all-footer-components/footer/footer.component';
 @Component({
   selector: 'app-contact-info',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule, FooterComponent],
   templateUrl: './contact-info.component.html',
   styleUrl: './contact-info.component.scss'
 })
