41fb4479279bfff8b09b8357979bef82a39a943a
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-17 16:50:51 +0200

    Contact Info Responsiveness added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { AboutMeComponent } from './main/about-me/about-me.component';
 import { SkillsComponent } from './main/skills/skills.component';
 import { TestimonialsComponent } from './main/testimonials/testimonials.component';
 import { ContactInfoComponent } from './main/contact-info/contact-info.component';
+import { FooterComponent } from './shared/all-footer-components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, MyProjectsComponent, AboutMeComponent, SkillsComponent, TestimonialsComponent, ContactInfoComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, MyProjectsComponent, AboutMeComponent, SkillsComponent, TestimonialsComponent, ContactInfoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
