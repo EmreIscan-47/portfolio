0da89daa355bcc3c7bace2a16c36706d0d39d85c
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-18 20:55:51 +0200

    Keyframe Animation added to About-me, Skills, My Project and Testimonials Section. Responsiveness added to Header

--- src/app/shared/all-header-components/header/header.component.ts
+++ src/app/shared/all-header-components/header/header.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   yellowlogo = false;
   dropDownHover = false;
+  dropDownActive = true;
+  styleStatus = 0;
+
+   toggleDropdown() {
+
+     this.styleStatus = this.styleStatus === 1 ? 2: 1;
+
+     if (this.styleStatus == 1) {
+      document.body.classList.add("dropdown-active");
+     } else {
+       document.body.classList.remove("dropdown-active");
+     }
+   }
 }
