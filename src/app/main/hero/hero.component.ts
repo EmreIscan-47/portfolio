8bc4d60f965731d265416b97ed2bcd5c089364fa
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-21 19:45:03 +0200

    Hello World roll out and roll in animation added

--- src/app/main/hero/hero.component.ts
+++ src/app/main/hero/hero.component.ts
@@ -14,6 +14,9 @@ export class HeroComponent {
   firstWrapper = ['F', 'r', 'o', 'n', 't', 'e', 'd'];
   secondWrapper = ['D', 'E', 'V','E','L', 'O','P','E', 'R'];
   hoveredIndex: Number | null = null;
+  helloWorldHover:boolean = false;
+  rollIn = false;
+
   onMouseEnter(i: Number) {
    this.hoveredIndex= i;
   }
