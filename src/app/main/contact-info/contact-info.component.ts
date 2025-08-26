1580b0b56ee33bc4b65bbb93641f1f146068c471
EmreIscan-47 <iscan.connect@outlook.de>
2025-08-26 15:10:53 +0200

    Add and complete form functionality

--- src/app/main/contact-info/contact-info.component.ts
+++ src/app/main/contact-info/contact-info.component.ts
@@ -1,55 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FooterComponent } from '../../shared/all-footer-components/footer/footer.component';
 import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contact-info',
   standalone: true,
-  imports: [FooterComponent, FormsModule],
+  imports: [FooterComponent, FormsModule, HttpClientModule, CommonModule],
   templateUrl: './contact-info.component.html',
   styleUrl: './contact-info.component.scss',
 })
-
 export class ContactInfoComponent {
   isButtonDisabled = true;
   isChecked = false;
   borderBlue: Number = 0;
+  formSubmitted = false;
+
+  namePlaceholder = 'Your name goes here';
+  emailPlaceholder = 'youremail@email.com';
+  messagePlaceholder = 'Hello Lukas, I am interested in...';
+
+  http = inject(HttpClient);
 
   contactData = {
-    name: "",
-    email: "",
-    message: "",
-    agree: false
-  }
+    name: '',
+    email: '',
+    message: '',
+    agree: false,
+  };
 
   toggleCheckbox() {
     let checkboxREF = document.getElementById('checkbox') as HTMLImageElement;
-    let filename = checkboxREF.src.substring(checkboxREF.src.lastIndexOf('/') + 1);
-   this.contactData.agree = !this.contactData.agree;
-    if (filename =='checkbox-checked.png') {
-      checkboxREF.src = './../../../assets/img/contact-info/checkbox-default.png';
+    let filename = checkboxREF.src.substring(
+      checkboxREF.src.lastIndexOf('/') + 1
+    );
+    this.contactData.agree = !this.contactData.agree;
+    if (filename == 'checkbox-checked.png') {
+      checkboxREF.src =
+        './../../../assets/img/contact-info/checkbox-default.png';
       this.isButtonDisabled = true;
     } else {
-      checkboxREF.src = './../../../assets/img/contact-info/checkbox-checked.png';
+      checkboxREF.src =
+        './../../../assets/img/contact-info/checkbox-checked.png';
       this.isButtonDisabled = false;
     }
   }
 
   borderActive(borderNumber: Number) {
-   this.borderBlue = borderNumber;
+    this.borderBlue = borderNumber;
   }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-         console.log(this.contactData);
+    this.formSubmitted = true;
+    this.trimAll();
+    if (ngForm.submitted && ngForm.form.valid) {
+      this.validForm(ngForm);
+    } else if (!ngForm.valid) {
+      this.notValidForm(ngForm);
+      ngForm.control.markAllAsTouched();
+      return;
     }
- 
-    
   }
 
-  
+  trimAll(){
+    this.contactData= {
+    name: this.contactData.name.trim(),
+    email: this.contactData.email?.trim(),
+    message: this.contactData.message.trim(),
+    agree: this.contactData.agree,
+  };
+  }
 
- disabledClick(){
-    console.log("yo");
-    
+  validForm(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
+
+  notValidForm(ngForm: NgForm) {
+    if (!this.contactData.name) {
+      this.namePlaceholder = 'Oops! it seems your name is missing ';
+    }
+    if (!this.contactData.email) {
+      this.contactData.email.trim();
+      this.emailPlaceholder = 'Hoppla! your email is required';
+    }
+    if (!this.contactData.message) {
+
+      this.messagePlaceholder = 'What do you need to develop?';
+    }
+  }
+
+  disabledClick() {
+    console.log('yo');
   }
 }
